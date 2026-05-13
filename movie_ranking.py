410dec9bbd65563afc8d3529cdf60074c0b62061
admin <admin@iMac-von-admin.fritz.box>
2026-05-13 13:25:16 +0200

    Lesson 1 for loops

---
+++ movie_ranking.py
@@ -0,0 +1,20 @@
+favorite_movies=[
+    'Schrek',
+    'Astral',
+    'Thomas Shelby',
+    'Rangers',
+    'Amnesia',
+]
+print('My favorite films:')
+for movie in favorite_movies:
+    print('-',movie)
+
+ranking_movies=[
+    'Interstellar',
+    'Dune',
+    'The Matrix',
+    'Spiderman',
+    'Brieth',
+]
+for i,movie in enumerate(ranking_movies):
+    print(i+1,'.',movie)
