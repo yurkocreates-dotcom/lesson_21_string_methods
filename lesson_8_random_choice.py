9001b0f7dfdcae29cf01c8668e7380479e12fddd
admin <admin@iMac-von-admin.fritz.box>
2026-05-13 21:59:30 +0200

    lesson_8_random_choice

---
+++ lesson_8_random_choice.py
@@ -0,0 +1,9 @@
+import random
+tools = ["ChatGPT", "Midjourney", "Claude","Notion AI", "Runway"]
+tool =random.choice(tools)
+print(tool)
+
+import random
+tools = ["ChatGPT", "Midjourney", "Claude","Notion AI", "Runway"]
+tool =random.choice(tools)
+print("Today's AI Tool:",tool)
