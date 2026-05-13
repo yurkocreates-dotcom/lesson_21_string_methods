a7093deec06365dcf7e12f1478160e46b7c83635
admin <admin@iMac-von-admin.fritz.box>
2026-05-13 22:13:04 +0200

    lesson_9_fstrings

--- lesson_8_random_choice.py
+++
@@ -1,9 +0,0 @@
-import random
-tools = ["ChatGPT", "Midjourney", "Claude","Notion AI", "Runway"]
-tool =random.choice(tools)
-print(tool)
-
-import random
-tools = ["ChatGPT", "Midjourney", "Claude","Notion AI", "Runway"]
-tool =random.choice(tools)
-print("Today's AI Tool:",tool)
---
+++ lesson_9_fstrings.py
@@ -0,0 +1,9 @@
+country="Germany"
+goal="AI Creator"
+print(f"I live in  {country}  and my goal is {goal} ")
+
+name="Yurii"
+country="Germany"
+goal="AI Creator"
+income_goal= "10000€"
+print(f"Name: {name} \n Country: {country} \n Goal: {goal} \n  Income Goal: { income_goal}")
