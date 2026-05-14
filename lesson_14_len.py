bd1a103c0d0c325540403bcac2ce99943d0fca5d
admin <admin@iMac-von-admin.fritz.box>
2026-05-14 13:37:01 +0200

    lesson_14_len

---
+++ lesson_14_len.py
@@ -0,0 +1,21 @@
+tools = [
+    "ChatGPT",
+    "Claude",
+    "Midjourney",
+    "Runway",
+    "Perplexity"
+]
+print(len(tools))
+
+
+
+tasks = [
+    "Learn Python",
+    "Build AI project",
+    "Push code to GitHub",
+    "Study German",
+    "Workout"
+]
+counter=len(tasks)
+
+print(  f"You have {counter} tasks today")
