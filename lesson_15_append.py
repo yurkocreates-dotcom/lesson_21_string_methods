22289f3ea5858170d66248076160286fbea467c7
admin <admin@iMac-von-admin.fritz.box>
2026-05-14 13:43:44 +0200

    lesson_15_append

---
+++ lesson_15_append.py
@@ -0,0 +1,17 @@
+tools = [
+    "ChatGPT",
+    "Claude",
+    "Midjourney",
+]
+tools.append("Midjourney")
+print(tools)
+tasks = [
+    "Learn Python",
+    "Build AI project"
+]
+tasks.append("Push code to GitHub")
+tasks.append( "Study German")
+tasks.append( "Workout")
+print(  "Today's Tasks:")
+for task in tasks:
+ print('-',task)
