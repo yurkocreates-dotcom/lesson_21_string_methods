0d7a359e2a49e8d10a4f9c5a4f7c7ce604b91250
admin <admin@iMac-von-admin.fritz.box>
2026-05-14 14:46:33 +0200

    lesson_20_in

---
+++ lesson_20_in.py
@@ -0,0 +1,21 @@
+countries = [
+    "Germany",
+    "Japan",
+    "Canada",
+    "USA",
+    "Ukraine"
+]
+if "Germany" in countries:
+    print("Country found")
+
+skills = [
+    "Python",
+    "AI",
+    "JavaScript",
+    "n8n",
+    "Automation"
+]
+if "Python"  in skills and "AI" in skills:
+  print("Skill found")
+else:
+   print("Skill missing")
